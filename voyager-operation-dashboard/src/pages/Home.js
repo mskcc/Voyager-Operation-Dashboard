200baf093fcabe12436708d7a922dfe57f267588
bfarahdel <britny205@gmail.com>
2022-10-20 12:45:07 -0400

    Added a pie chart for gene panel data.

--- voyager-operation-dashboard/src/pages/Home.js
+++ voyager-operation-dashboard/src/pages/Home.js
@@ -4,6 +4,7 @@ function Home() {
 
     const credentials = btoa("admin:correctHorseBatteryStaple")
     const [runStatus, setRunStatus] = useState({})
+    const [genePanel, setGenePanel] = useState({})
 
     useEffect(() => {
         fetch('http://localhost:8081/v0/run/api/?run_distribution=status', {
@@ -11,8 +12,16 @@ function Home() {
         })
         .then((r) => r.json())
         .then((data) => setRunStatus(data))
+
+        fetch('http://localhost:8081/v0/fs/distribution/?metadata_distribution=genePanel', {
+            headers: {'Authorization': `Basic ${credentials}`}
+        })
+        .then((r) => r.json())
+        .then((data) => setGenePanel(data))
+
     }, [])
     
+    // Run Status Data
     const processedRunData = [
         ["Status", "Count"],
         ["Creating", runStatus[0]],
@@ -28,10 +37,26 @@ function Home() {
         is3D: true,
     };
 
+    // Gene Panel Data
+    const processedGeneData = [
+        ["Gene", "Count"],
+        ["IMPACT505", genePanel["IMPACT505"]],
+        ["ShallowWGS", genePanel["ShallowWGS"]],
+        ["CMO-CH", genePanel["CMO-CH"]],
+        ["HumanWholeGenome", genePanel["HumanWholeGenome"]],
+        ["ACCESS", genePanel["ACCESS"]],
+        ["WholeExome", genePanel["WholeExome"]]
+    ];
+
+    const geneOptions = {
+        title: "Gene Panel",
+        is3D: true,
+    }
+
     return (
         <div className="home-container">
             <PieChart data={processedRunData} options={runOptions}/>
-            <PieChart data={processedRunData} options={runOptions}/>
+            <PieChart data={processedGeneData} options={geneOptions}/>
         </div>
     )
 }
