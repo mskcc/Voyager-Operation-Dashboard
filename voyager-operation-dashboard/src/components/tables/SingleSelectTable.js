01ae8aa4d9bcaf5b6bbe30613237577759089b02
RaccoonNW <87783832+RaccoonNW@users.noreply.github.com>
2022-11-01 14:45:15 -0600

    Removed select all checkbox

--- voyager-operation-dashboard/src/components/tables/SingleSelectTable.js
+++ voyager-operation-dashboard/src/components/tables/SingleSelectTable.js
@@ -2,19 +2,24 @@ import { DataGrid } from '@mui/x-data-grid';
 
 function SingleSelectTable({ columns, rows, selection }) {
 
-    return (
-        <div style={{ height: 400, width: '100%' }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={20}
-            rowsPerPageOptions={[20]}
-            // onRowClick={handleRowClick}
-            checkboxSelection
-            onSelectionModelChange={selection}
-          />
-        </div>
-    );
+  return (
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          sx={{
+            "& .MuiDataGrid-columnHeaderCheckbox .MuiDataGrid-columnHeaderTitleContainer": {
+              display: "none"
+            }
+          }}
+          rows={rows}
+          columns={columns}
+          pageSize={20}
+          rowsPerPageOptions={[20]}
+          // onRowClick={handleRowClick}
+          checkboxSelection
+          onSelectionModelChange={selection}
+        />
+      </div>
+  );
 };
 
 export default SingleSelectTable
